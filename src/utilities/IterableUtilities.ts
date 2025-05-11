ce5c097c0a032f1e95a35cc948f281280216e1a2
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2025-05-11 14:33:59 +0200

    Enable stricter type-safety options
    `exactOptionalPropertyTypes` and `noUncheckedIndexedAccess` are enabled
    by default in the TypeScript presets.

--- src/utilities/IterableUtilities.ts
+++ src/utilities/IterableUtilities.ts
@@ -9,16 +9,17 @@ export function indexOfFromBinarySearch(
 		const middleIndex = Math.floor(
 			(inclusiveStartIndex + inclusiveEndIndex) / 2,
 		)
-		const comparison = target.localeCompare(sortedValues[middleIndex], "en")
+		const comparison = target.localeCompare(
+			sortedValues[middleIndex] ?? "",
+			"en",
+		)
 
 		if (comparison === 0) {
 			return middleIndex
 		}
-
 		if (inclusiveStartIndex >= inclusiveEndIndex) {
 			return -1
 		}
-
 		return comparison < 0
 			? binarySearch(inclusiveStartIndex, middleIndex - 1)
 			: binarySearch(middleIndex + 1, inclusiveEndIndex)
--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,68 @@
 import { join as joinPath, resolve as resolvePath } from "node:path"
 import { fileURLToPath } from "node:url"
-import { defineConfig } from "vitest/config"
-import tsconfigJson from "./tsconfig.json"
+import { type ViteUserConfig as ViteConfig, defineConfig } from "vitest/config"
+import tsconfigJson from "./tsconfig.json" assert { type: "json" }
 
 const projectDirectory = joinPath(fileURLToPath(import.meta.url), "..")
 
-export default defineConfig(() => ({
-	build: {
-		emptyOutDir: true,
-		minify: "esbuild" as const,
-		reportCompressedSize: false,
-		rollupOptions: {
-			output: {
-				entryFileNames: "main.mjs",
+export default defineConfig(() => {
+	const baseConfiguration: ViteConfig = {
+		build: {
+			emptyOutDir: true,
+			minify: "esbuild" as const,
+			reportCompressedSize: false,
+			rollupOptions: {
+				output: {
+					entryFileNames: "main.mjs",
+				},
 			},
 		},
-	},
-	cacheDir: inProjectDirectory("node_modules/.cache/"),
-	plugins: [],
-	resolve: {
-		alias: getAliasesFromTsconfig(),
-	},
-	ssr: {
-		noExternal: ["@actions/core", "@actions/github", "undici", "zod"],
-	},
-	test: {
-		coverage: {
-			include: ["src/**/*.ts"],
-			exclude: ["src/**/*.tests.ts"],
-			provider: "v8" as const,
-			reportsDirectory: inProjectDirectory(
-				"node_modules/.cache/vitest/coverage/",
-			),
+		cacheDir: inProjectDirectory("node_modules/.cache/"),
+		plugins: [],
+		resolve: {
+			alias: tsconfigPathAliases(),
 		},
-		include: ["src/**/*.tests.ts"],
-	},
-}))
+		ssr: {
+			noExternal: ["@actions/core", "@actions/github", "undici", "zod"],
+		},
+		test: {
+			coverage: {
+				include: ["src/**/*.ts"],
+				exclude: ["src/**/*.tests.ts"],
+				provider: "v8" as const,
+				reportsDirectory: inProjectDirectory(
+					"node_modules/.cache/vitest/coverage/",
+				),
+			},
+			include: ["src/**/*.tests.ts"],
+		},
+	}
 
-function getAliasesFromTsconfig(): Record<string, string> {
+	return baseConfiguration
+})
+
+function tsconfigPathAliases(): Record<string, string> {
 	return Object.fromEntries(
-		Object.entries(tsconfigJson.compilerOptions.paths).map(
-			([alias, [path]]) => [
+		Object.entries(tsconfigJson.compilerOptions.paths).map((entry) => {
+			assertSinglePath(entry)
+			const [alias, [path]] = entry
+			return [
 				alias.slice(0, -"/*".length),
 				inProjectDirectory(path.slice(0, -"/*".length)),
-			],
-		),
+			]
+		}),
 	)
+}
+
+function assertSinglePath(
+	entry: [alias: string, paths: Array<string>],
+): asserts entry is [alias: string, paths: [string]] {
+	const [alias, paths] = entry
+	if (paths.length !== 1) {
+		throw new Error(
+			`Path alias '${alias}' in 'tsconfig.json' must specify exactly one path, but has ${paths.length} paths`,
+		)
+	}
 }
 
 function inProjectDirectory(relativePath: string): string {
