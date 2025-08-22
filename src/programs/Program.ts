4bd5a74639552ccd079b258908db790ae6b0c898
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2025-08-22 14:52:01 +0200

    Introduce configurations

--- src/programs/Program.ts
+++ src/programs/Program.ts
@@ -1,10 +1,17 @@
 import { getCommits } from "#commits/GetCommits"
+import { getConfiguration } from "#configurations/GetConfiguration"
 import { EXIT_CODE_GENERAL_ERROR, type ExitCode } from "#types/ExitCode"
 import { printMessage } from "#utilities/logging/Logger"
 
 export async function program(_args: Array<string>): Promise<ExitCode> {
-	const commits = await getCommits()
-	printMessage(commits.join(" "))
+	const [configuration, commits] = await Promise.all([
+		getConfiguration(),
+		getCommits(),
+	])
+
+	printMessage(JSON.stringify(configuration))
+	printMessage(JSON.stringify(commits))
 	printMessage("Not implemented yet")
+
 	return EXIT_CODE_GENERAL_ERROR
 }
