b40bd48174beb8d29c34796604795bf79072e55b
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2025-11-22 18:50:20 +0100

    Introduce a refined `Commit` type

--- src/utilities/Arrays.ts
+++ src/utilities/Arrays.ts
@@ -10,3 +10,7 @@ export function* splitToChunks<Item>(
 export function notNullish<Item>(item: Item | null | undefined): item is Item {
 	return item !== null && item !== undefined
 }
+
+export function notEmptyString(item: string): boolean {
+	return item !== ""
+}
