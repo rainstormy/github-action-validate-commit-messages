4577bcf7028ed36b50b684faca017b80e893f5d0
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2026-02-07 11:10:39 +0100

    Retrieve commits from the local Git log

--- src/utilities/Arrays.ts
+++ src/utilities/Arrays.ts
@@ -21,3 +21,11 @@ export function notNullishValue<Item>(
 export function notEmptyString<Item>(item: Item): boolean {
 	return item !== ""
 }
+
+export type NonEmptyArray<Item> = [Item, ...Array<Item>]
+
+export function isNonEmptyArray<Item>(
+	items: Array<Item>,
+): items is NonEmptyArray<Item> {
+	return items.length > 0
+}
---
+++ src/utilities/git/cli/GetGitLog.ts
@@ -0,0 +1,115 @@
+import type { NonEmptyArray } from "#utilities/Arrays.ts"
+import type { GitLogCommitDto } from "#utilities/git/cli/dtos/GitLogCommitDto.ts"
+import { runGitCommand } from "#utilities/git/cli/RunGitCommand.ts"
+
+export async function getGitLog(
+	fromRef: string,
+	toRef: string,
+): Promise<Array<GitLogCommitDto>> {
+	const range = `${fromRef}..${toRef}`
+	const output = await runGitCommand([
+		"--no-pager", // Print all output at once instead of using pagination.
+		"log",
+		"--format=raw",
+		"--no-color",
+		range,
+	])
+
+	if (output === "") {
+		return []
+	}
+
+	const lines = output.split("\n")
+	const [firstLine, ...remainingLines] = lines
+
+	assertCommitLine(firstLine, range)
+	return mapLogLinesToCommitDtos(firstLine, remainingLines)
+}
+
+function mapLogLinesToCommitDtos(
+	firstLine: `commit ${string}`,
+	remainingLines: Array<string>,
+): Array<GitLogCommitDto> {
+	const commitDtos: Array<GitLogCommitDto> = []
+
+	let currentCommitDto = gitLogCommitDto(firstLine.slice("commit ".length))
+	let currentField: NonEmptyArray<string> = currentCommitDto.commit
+
+	function addCommitDto(dto: GitLogCommitDto): void {
+		commitDtos.push(trimTrailingNewlines(dto))
+	}
+
+	function addLine(line: string): void {
+		currentField[currentField.length - 1] += `${line}\n`
+	}
+
+	for (const line of remainingLines) {
+		if (line.startsWith("commit ")) {
+			addCommitDto(currentCommitDto)
+			currentCommitDto = gitLogCommitDto(line.slice("commit ".length))
+			currentField = currentCommitDto.commit
+
+			// A commit message.
+		} else if (line.startsWith("    ")) {
+			currentField = currentCommitDto.message
+			addLine(line.slice("    ".length))
+
+			// A multiline value.
+		} else if (line.startsWith(" ")) {
+			addLine(line.slice(" ".length))
+
+			// A key-value field.
+		} else if (line.includes(" ")) {
+			const valueIndex = line.indexOf(" ")
+
+			const key = line.slice(0, valueIndex)
+			const value = `${line.slice(valueIndex + " ".length)}\n`
+
+			if (currentCommitDto[key] === undefined) {
+				currentCommitDto[key] = [value]
+			} else {
+				currentCommitDto[key].push(value)
+			}
+			currentField = currentCommitDto[key] as NonEmptyArray<string>
+
+			// A blank line.
+		} else {
+			addLine("")
+		}
+	}
+
+	addCommitDto(currentCommitDto)
+	return commitDtos
+}
+
+function gitLogCommitDto(commitSha: string): GitLogCommitDto {
+	return {
+		author: [],
+		commit: [`${commitSha}\n`],
+		committer: [],
+		message: [""],
+		parent: [],
+	}
+}
+
+function trimTrailingNewlines(dto: GitLogCommitDto): GitLogCommitDto {
+	return Object.fromEntries(
+		Object.entries(dto).map(([key, values]) => [
+			key,
+			values.map((value) =>
+				value.endsWith("\n\n")
+					? value.slice(0, -"\n\n".length)
+					: value.slice(0, -"\n".length),
+			),
+		]),
+	) as GitLogCommitDto
+}
+
+function assertCommitLine(
+	line: string | undefined,
+	logRange: string,
+): asserts line is `commit ${string}` {
+	if (!line?.startsWith("commit ")) {
+		throw new Error(`Unexpected line in the Git log of ${logRange}: ${line}`)
+	}
+}
