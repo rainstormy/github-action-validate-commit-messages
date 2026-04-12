814f317953a54c01c1e7d9d4546345250ca87a99
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2026-04-12 22:52:05 +0200

    Scaffold all remaining rule functions

--- src/domains/rules/NoUnexpectedPunctuation.ts
+++ src/domains/rules/NoUnexpectedPunctuation.ts
@@ -1,7 +1,18 @@
-import type { Commits } from "#commits/Commit.ts"
-import type { Concerns } from "#rules/concerns/Concern.ts"
+import type { Commit, Commits } from "#commits/Commit.ts"
+import type { Concern, Concerns } from "#rules/concerns/Concern.ts"
+import { type RuleContext, ruleContext } from "#rules/Rule.ts"
 import type { EmptyObject } from "#types/EmptyObject.ts"
+import { notNullish } from "#utilities/Arrays.ts"
 
-export function noUnexpectedPunctuation(_commits: Commits, _options: EmptyObject | null): Concerns {
+export function noUnexpectedPunctuation(commits: Commits, options: EmptyObject | null): Concerns {
+	if (options === null) {
+		return []
+	}
+
+	const rule = ruleContext("noUnexpectedPunctuation")
+	return commits.map((commit) => verifyCommit(commit, rule)).filter(notNullish)
+}
+
+function verifyCommit(_commit: Commit, _rule: RuleContext): Concern | null {
 	throw new Error("The `noUnexpectedPunctuation` rule has not been implemented yet") // TODO: To be implemented.
 }
--- src/domains/rules/UseEmptyLineBeforeBodyLines.ts
+++ src/domains/rules/UseEmptyLineBeforeBodyLines.ts
@@ -1,10 +1,21 @@
-import type { Commits } from "#commits/Commit.ts"
-import type { Concerns } from "#rules/concerns/Concern.ts"
+import type { Commit, Commits } from "#commits/Commit.ts"
+import type { Concern, Concerns } from "#rules/concerns/Concern.ts"
+import { type RuleContext, ruleContext } from "#rules/Rule.ts"
 import type { EmptyObject } from "#types/EmptyObject.ts"
+import { notNullish } from "#utilities/Arrays.ts"
 
 export function useEmptyLineBeforeBodyLines(
-	_commits: Commits,
-	_options: EmptyObject | null,
+	commits: Commits,
+	options: EmptyObject | null,
 ): Concerns {
+	if (options === null) {
+		return []
+	}
+
+	const rule = ruleContext("useEmptyLineBeforeBodyLines")
+	return commits.map((commit) => verifyCommit(commit, rule)).filter(notNullish)
+}
+
+function verifyCommit(_commit: Commit, _rule: RuleContext): Concern | null {
 	throw new Error("The `useEmptyLineBeforeBodyLines` rule has not been implemented yet") // TODO: To be implemented.
 }
